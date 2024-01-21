ea988d22a92ce0bb6b5f9bcb951443b3c500cc74
dikshya2002 <ddikshya.agarwal2002@gmail.com>
2024-01-21 18:34:42 +0530

    [added first day]

---
+++ p1/app1/src/App.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         My name is Dikshya
+        </p>
+        <button>Change Name</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
